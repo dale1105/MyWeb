df701543c7196f8137f394ab4ea38c26282a8a34
dale-pc <dale-pc@DESKTOP-5DNEA48>
2017-07-26 23:37:49 +0800

    第一个web程序

---
+++ MyWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyWebProject.Global" Language="C#" %>
---
+++ MyWebProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebProject.Startup))]
+namespace MyWebProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
